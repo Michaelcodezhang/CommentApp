397f28188108600b5b9edcbd822b214642b6f53e
michaelcode <michaelcode@localhost.localdomain>
2017-08-16 10:35:46 +0800

    using react-redux

---
+++ src/components/CommentInput.js
@@ -0,0 +1,81 @@
+import React,{Component} from 'react'
+import PropTypes from 'prop-types'
+
+class CommentInput extends Component{
+  static propTypes={
+    username:PropTypes.any,
+    onSubmit:PropTypes.func,
+    onUserNameInputBlur:PropTypes.func
+  }
+  static defaultProps={
+    username:''
+  }
+  constructor (props){
+    super(props)
+    this.state={
+      username:props.username,
+      content:''
+    }
+  }
+  componentDidMount(){
+    this.textarea.focus()
+  }
+  handleUsernameBlur(event){
+    if(this.props.onUserNameInputBlur){
+      this.props.onUserNameInputBlur(event)
+    }
+  }
+  handleUsernameChang(event){
+    this.setState({
+      username:event.target.value
+    })
+  }
+  handleContentChang(event){
+    this.setState({
+      content:event.target.value
+    })
+  }
+  handleSubmit(){
+    if(this.props.onSubmit){
+      this.props.onSubmit({
+        username:this.state.username,
+        content:this.state.content,
+        createdTime:+new Date()
+      })
+    }
+    this.setState({content:''})
+  }
+  render(){
+    return(
+      <div className="comment-input">
+        <div className="comment-field">
+          <span className="comment-field-name">用户名：</span>
+          <div className="comment-field-input">
+            <input
+              value={this.state.username}
+              onBlur={this.handleUsernameBlur.bind(this)}
+              onChange={this.handleUsernameChang.bind(this)}/>
+          </div>
+        </div>
+        <div className="comment-field">
+          <span className="comment-field-name">评论内容：</span>
+          <div className="comment-field-input">
+            <textarea
+              value={this.state.content}
+              ref={(textarea)=>{this.textarea=textarea}}
+              onChange={this.handleContentChang.bind(this)}>
+            </textarea>
+          </div>
+        </div>
+        <div className="comment-field-button">
+          <button
+          onClick={this.handleSubmit.bind(this)}>
+            发布
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CommentInput
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentApp from './CommentApp'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comment'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CommentApp/>,document.getElementById('root'))
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+const store=createStore(commentsReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CommentApp/>
+  </Provider>,
+  document.getElementById('root')
+)
+
+
